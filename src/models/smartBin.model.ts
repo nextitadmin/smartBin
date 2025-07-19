ba3e54c7017fd0f2608817febe55a87bea18ae22
utzmankazeem <usman4kay@gmail.com>
2025-07-19 21:35:30 +0100

    feat: smartbin model definition

---
+++ src/models/smartBin.model.ts
@@ -0,0 +1,156 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes, Types } from 'mongoose';
+
+export enum SmartbinStatus {
+  Pending = 'pending',
+  Approved = 'approved',
+  Diclined = 'declined',
+}
+
+export enum BinType {
+  Smart = 'smart',
+  Non_Smart = 'non_smart',
+}
+
+export enum LAWMACustomerType {
+  Returning = 'Returning',
+  New = 'New',
+}
+
+export enum CustomerType {
+  Resident = 'Resident',
+  Corporate = 'Corporate',
+  Facility = 'Facility',
+  Agent = 'Agent',
+}
+
+export enum PaymentMethod {
+  AlatByWema = 'Alat',
+  Wallet = 'wallet',
+}
+export interface SmartBinAttributes {
+  _id?: string;
+  userId: Types.ObjectId;
+  payerId: string;
+  binType: BinType;
+  status: SmartbinStatus;
+  customerType: CustomerType;
+  lawmaCustomerType?: LAWMACustomerType;
+  paymentMethod?: PaymentMethod;
+  buildingName?: string;
+  address?: string;
+  businessType?: string;
+  email?: string;
+  phoneNumber?: string;
+  amount?: number;
+  branch?: string;
+  closestLandmark?: string;
+  name?: string;
+  businessName?: string;
+  buildingType?: string;
+  houseName?: string;
+  flatNumber?: string;
+  localGovernmentArea?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+@Schema({
+  collection: 'smart_bins',
+  timestamps: true,
+  versionKey: false,
+})
+export class SmartBin extends Document {
+    @Prop({
+        type: SchemaTypes.ObjectId,
+        required: true,
+    })
+    userId: Types.ObjectId;
+    
+    @Prop({
+        type: SchemaTypes.String,
+        required: true,
+    })
+    payerId: string;
+    
+    @Prop({
+        type: String,
+        enum: Object.values(BinType),
+        default: BinType.Smart,
+    })
+    binType: BinType;
+    
+    @Prop({
+        type: String,
+        enum: Object.values(SmartbinStatus),
+        default: SmartbinStatus.Pending,
+    })
+    status: SmartbinStatus;
+    
+    @Prop({
+        type: String,
+        enum: Object.values(CustomerType),
+        required: true,
+    })
+    customerType: CustomerType;
+    
+    @Prop({
+        type: String,
+        enum: Object.values(LAWMACustomerType),
+        default: LAWMACustomerType.New,
+    })
+    lawmaCustomerType?: LAWMACustomerType;
+    
+    @Prop({
+        type: String,
+        enum: Object.values(PaymentMethod),
+        default: PaymentMethod.Wallet,
+    })
+    paymentMethod?: PaymentMethod;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    buildingName?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    address?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    businessType?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    email?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    phoneNumber?: string;
+    
+    @Prop({ type: SchemaTypes.Number, required: false })
+    amount?: number;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    branch?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    closestLandmark?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    name?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    businessName?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    buildingType?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    houseName?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    flatNumber?: string;
+    
+    @Prop({ type: SchemaTypes.String, required: false })
+    localGovernmentArea?: string;
+}
+
+export const SmartBinSchema = SchemaFactory.createForClass(SmartBin);
+
+
+
