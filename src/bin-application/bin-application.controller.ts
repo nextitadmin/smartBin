3b9f74a04b1585ebe3808800603abef9797cd263
utzmankazeem <usman4kay@gmail.com>
2025-07-23 17:47:25 +0100

    feat: services created

--- src/bin-application/bin-application.controller.ts
+++ src/bin-application/bin-application.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { BinApplicationService } from './bin-application.service';
+import { SmartBin } from '@models/smart-bin.model';
+import { BinAppDto } from './dto/binAppDto';
 
 @Controller('bin-application')
-export class BinApplicationController {}
+export class BinApplicationController {
+    constructor(private readonly binApplicationService: BinApplicationService) {}
+    
+    // get all bin applications
+    @Get()
+    async getAllBinApplications(@Req() req, @Res() res) {
+        const binApplications = await this.binApplicationService.getAllBinApplications();
+        return res.status(200).json({
+            message: 'Bin applications retrieved successfully',
+            data: binApplications,
+        });
+    }
+    
+}
--- src/bin-application/bin-application.service.ts
+++ src/bin-application/bin-application.service.ts
@@ -5,7 +5,7 @@ import { Injectable,
 
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { SmartBin } from '@models/smart-bin.model';
+import { BinType, CustomerType, SmartBin } from '@models/smart-bin.model';
 import { Resident } from '@models/users/resident.model';
 import { Agent } from '@models/users/agent.model';
 import { Corporate } from '@models/users/corporate.model';
@@ -13,6 +13,7 @@ import { FacilityManager } from '@models/users/facility-manager.model';
 import { Bill } from '@models/bill.model';
 import { Wallet } from '@models/wallet.model';
 import { Transaction } from '@models/transaction.model';
+import { BinAppDto } from './dto/binAppDto';
 
 @Injectable()
 export class BinApplicationService {
@@ -41,22 +42,14 @@ export class BinApplicationService {
                 throw new BadRequestException('Invalid user type');
         }
     }
+    // For Resident
     async getResidentBinApplication(userId: string) {
-        const [
-            resident,
-            bills,
-            wallet,
-            smartbin,
-            // pickups,
-            // disposals,
-        ] = await Promise.all([
+        const [resident,bills,wallet,smartbin] = await Promise.all([
             this.residentModel.findById(userId).lean(),
             this.billModel.find({ userId, userType: 'Resident' }).lean(),
             this.walletModel.findOne({ userId, userType: 'Resident' }).lean(),
             this.smartbinModel
             .findOne({ userId, userType: 'Resident' }).sort({ createdAt: -1 }).lean(),
-            // this.pickupModel.find({ userId, userType: 'Resident' }).lean(),
-            // this.disposalModel.find({ userId, userType: 'Resident' }).lean(),
         ]);
         if (!resident) {
             throw new NotFoundException('Resident not found');
@@ -65,20 +58,218 @@ export class BinApplicationService {
             throw new NotFoundException('No bin application found for this resident');
         }
         return {
-            orderId: smartbin[0]._id,
-            userId: smartbin[0].userId,
+            orderId: smartbin._id,
+            Surname: resident?.firstName,
+            firstName: resident?.lastName,
+            userId: smartbin.userId,
             userType: 'Resident',
             date: smartbin[0].createdAt,
-            status: smartbin[0].status,
-            amount: smartbin[0].amount,
-            binType: smartbin[0].binType,
-            deliveredOn: smartbin[0]?.deliveredOn,
-            deliveredBy: smartbin[0]?.deliveredBy,
-            address: smartbin[0]?.address,
+            status: smartbin.status || 'none',
+            amount: smartbin.amount,
+            binType: smartbin.binType,
+            deliveredOn: smartbin[0].deliveredOn,
+            deliveredBy: smartbin[0].deliveredBy,
+            address: smartbin?.address,
             approvalDate: smartbin[0].approvalDate,
             resident,
             bills,
             wallet,
             smartbin,
         };        
+    }
+    // For FAcilityManager
+    async getFacilityManagerBinApplication(facilityManagerId: string) {
+        const [facilityManager,bills,wallet,smartbin] = await Promise.all([
+            this.facilityModel.findById(facilityManagerId).lean(),
+            this.billModel.find({ 
+                userId: facilityManagerId, userType: 'Facility' }).lean(),
+            this.walletModel.findOne({ 
+                userId: facilityManagerId, userType: 'Facility' }).lean(),
+            this.smartbinModel
+            .findOne({ userId: facilityManagerId, userType: 'Facility' }).sort({ createdAt: -1 }).lean(),
+       ]);
+        if (!facilityManager) {
+            throw new NotFoundException('Facility Manager not found');
+        }
+        if (!smartbin) {
+            throw new NotFoundException('No bin application found for this facility manager');
+        }
+        return {
+            id: smartbin._id,
+            userId: smartbin.userId,
+            userType: 'Facility',
+            fullName: `${facilityManager?.firstName || ' ', facilityManager?.lastName || ' '}`,
+            buildingName: smartbin.buildingName,
+            buildingType: smartbin.buildingType,
+            addressOfFacility: smartbin.address,
+            closestLandmark: smartbin.closestLandmark,
+            facilityManager,
+            bills,
+            wallet,
+            smartbin,
+        };
+    }
+    // For Agent
+    async getAgentBinApplication(agentId: string) {
+        const [agent,bills,wallet,smartbin ] = await Promise.all([
+            this.agentModel.findById(agentId).lean(),
+            this.billModel.find({ userId: agentId, userType: 'Agent' }).lean(),
+            this.walletModel.findOne({ userId: agentId, userType: 'Agent' }).lean(),
+            this.smartbinModel
+            .findOne({ userId: agentId, userType: 'Agent' }).sort({ createdAt: -1 }).lean(),
+        ]);
+        if (!agent) {
+            throw new NotFoundException('Agent not found');
+        }
+        if (!smartbin) {
+            throw new NotFoundException('No bin application found for this agent');
+        }
+        return {
+            id: smartbin._id,
+            userId: smartbin.userId,
+            userType: 'Agent',
+            Surname: agent?.firstName,
+            firstName: agent?.lastName,
+            Email: smartbin.email,
+            Phone: smartbin.phoneNumber,
+            CustomerType: smartbin.customerType,
+            businessName: smartbin.businessName,
+            businessType: smartbin.businessType,
+            localGovernmentArea: smartbin.localGovernmentArea,
+            colsestLandmark: smartbin.closestLandmark,
+            companyAddress: smartbin.address,
+            status: smartbin.status || 'none',
+            date: smartbin[0]?.createdAt,
+            amount: smartbin.amount,
+            binType: smartbin.binType,
+            agent,
+        };
+    }    
+    // For Corporate
+    async getCorporateBinApplication(corporateId: string) {
+        const [corporate,bills,wallet,smartbin] = await Promise.all([
+            this.corporateModel.findById(corporateId).lean(),
+            this.billModel.find({ userId: corporateId, userType: 'Corporate' }).lean(),
+            this.walletModel.findOne({ userId: corporateId, userType: 'Corporate' }).lean(),
+            this.smartbinModel
+            .findOne({ userId: corporateId, userType: 'Corporate' }).sort({ createdAt: -1 }).lean(),
+        ]);
+        if (!corporate) {
+            throw new NotFoundException('Corporate not found');
+        }
+        if (!smartbin) {
+            throw new NotFoundException('No bin application found for this corporate');
+        }
+        return {
+            id: smartbin._id,
+            userId: smartbin.userId,
+            userType: 'Corporate',
+            Surname: corporate?.firstName,
+            firstName: corporate?.lastName,
+            Email: smartbin.email,
+            Phone: smartbin.phoneNumber,
+            CustomerType: smartbin.customerType,
+            businessName: smartbin.businessName,
+            businessType: smartbin.businessType,
+            localGovernmentArea: smartbin.localGovernmentArea,
+            colsestLandmark: smartbin.closestLandmark,
+            companyAddress: smartbin.address,
+            status: smartbin.status || 'none',
+            date: smartbin[0]?.createdAt,
+            amount: smartbin.amount,
+            binType: smartbin.binType,
+            corporate,
+        };
+    }
+    // Estimate annual subscription based on the bills
+    private estimateAnnualSubscription(bills: Bill[]): number { 
+        const total = bills.reduce((sum, bill) => sum + bill.amount, 0);
+        return total * 12; // Assuming the bills are monthly
+    }  
+    // Get all bin applications
+    async getAllBinApplications() {
+        const smartbins = await this.smartbinModel.find().sort({ createdAt: -1 }).lean();
+        if (!smartbins || smartbins.length === 0) {
+            throw new NotFoundException('No bin applications found');
+        }
+        return smartbins;
+    }   
+    // Get bin application by ID
+    async getBinApplicationById(id: string) {
+        const smartbin = await this.smartbinModel.findById(id).lean();
+        if (!smartbin) {
+            throw new NotFoundException('Bin application not found');
+        }
+        return smartbin;
+    }   
+    // Update bin application status
+    async updateBinApplicationStatus(id: string, status: string) {
+        const smartbin = await this.smartbinModel.findByIdAndUpdate(id, { status }, { new: true }). lean();
+        if (!smartbin) {
+            throw new NotFoundException('Bin application not found');
+        }
+        return smartbin;
+    }
+
+    // Delete bin application
+    async deleteBinApplication(id: string) {
+        const smartbin = await this.smartbinModel.findByIdAndDelete(id).lean();
+        if (!smartbin) {
+            throw new NotFoundException('Bin application not found');
+        }
+        return { message: 'Bin application deleted successfully' };
+    }
+    // Create a new bin application
+    async createBinApplication(dto: BinAppDto) {
+        const { userId, binType, customerType, payerId, address, amount, status,
+            paymentMethod, buildingName, businessType, email, phoneNumber, branch, 
+            closestLandmark, name, businessName, buildingType, houseName, flatNumber, 
+            localGovernmentArea
+          } = dto;
+
+        // Check if the user already has a bin application
+        const existingApplication = await this.smartbinModel.findOne({ userId }).lean();
+        if (existingApplication) {
+            throw new BadRequestException('User already has a bin application');
+        }
+        // Create a new bin application
+        const newBinApplication = new this.smartbinModel({
+            userId: new Types.ObjectId(userId),
+            binType: binType || BinType.Smart,
+            customerType: customerType || CustomerType.Resident,
+            payerId: payerId,
+            address: address || '',
+            amount: amount || 0,
+            status: status || 'Pending',
+            paymentMethod: paymentMethod,
+            buildingName: buildingName,
+            businessType: businessType,
+            email: email,
+            phoneNumber: phoneNumber,
+            branch: branch,
+            closestLandmark: closestLandmark,
+            name: name,
+            businessName: businessName,
+            buildingType: buildingType,
+            houseName: houseName,
+            flatNumber: flatNumber,
+            localGovernmentArea: localGovernmentArea,
+            approvalDate: new Date(), // Set approval date to now
+        });
+        // If the bin type is not specified, default to SmartBin
+        if (!binType) {
+            newBinApplication.binType = BinType.Smart;
+        }    
+        // Save the new bin application
+        await newBinApplication.save();
+        return newBinApplication;
+    }
+    // Get bin applications by user ID
+    async getBinApplicationsByUserId(userId: string) {
+        const smartbins = await this.smartbinModel.find({ userId }).sort({ createdAt: -1 }).lean();
+        if (!smartbins || smartbins.length === 0) {
+            throw new NotFoundException('No bin applications found for this user');
+        }
+            return smartbins;
+        }
     }
