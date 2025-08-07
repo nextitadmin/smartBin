0982e3f7fe95f32c3fb9dc0b110c647570ec3806
David Adediji <davidadediji@gmail.com>
2025-08-07 21:50:50 +0100

    update corporate dashboard endpoint

--- src/corporate/dashboard/dashboard.controller.ts
+++ src/corporate/dashboard/dashboard.controller.ts
@@ -10,14 +10,14 @@ import { ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Dashboard')
 @Controller({
-  path: 'dashboard',
+  path: 'corporates',
   version: '1',
 })
 export class DashboardController {
   constructor(private readonly dasboard: DashboardService) { }
 
   @CorporateAuth()
-  @Get('corporate')
+  @Get('dashboard')
   @ApiQuery({ name: 'year', required: false, type: Number })
   async getCorporateDashboard(@AuthenticatedCorporate() corporate: AuthUser, @Query('year') year?: number,) {
     const queryYear = year || new Date().getFullYear();
