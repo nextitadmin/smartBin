0a545ce6a5ec5581c909588eea21110ab1d51f28
Gabriel Adewumi <codergab@gmail.com>
2025-08-14 11:31:35 +0100

    added payment to smartbin

--- src/resident/report.controller.ts
+++ src/resident/report.controller.ts
@@ -1,75 +1,78 @@
 import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
 import {
-    AuthenticatedResident,
-    ResidentAuth,
+  AuthenticatedResident,
+  ResidentAuth,
 } from '@common/decorators/auth.decorator';
 import { ResidentUser } from '@common/types';
 import { ReportService } from '@src/report/report.service';
 import {
-    CreateReportDto,
-    GetReportsDto,
-    ReportResponseDto,
-    FullReportResponseDto,
+  CreateReportDto,
+  GetReportsDto,
+  ReportResponseDto,
+  FullReportResponseDto,
 } from '@src/report/dtos/report.dto';
 import { ReportType } from '@models/report.model';
 import { ApiQuery } from '@nestjs/swagger';
 import { SuccessResponse } from '@common/http';
 import {
-    ApiBearerAuth,
-    ApiTags,
-    ApiOperation,
-    ApiResponse,
-    ApiParam,
+  ApiBearerAuth,
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
 } from '@nestjs/swagger';
 
 @ApiTags('Resident- Reports')
 @Controller({
-    path: 'resident/reports',
-    version: '1',
+  path: 'resident/reports',
+  version: '1',
 })
 @ResidentAuth()
 export class ResidentReportController {
-    constructor(private readonly reportService: ReportService) { }
+  constructor(private readonly reportService: ReportService) {}
 
-    @Post()
-    @ApiResponse({
-        status: 201,
-        description: 'Report generated successfully',
-        type: ReportResponseDto,
-    })
-    async generateReport(
-        @Body() dto: CreateReportDto,
-        @AuthenticatedResident() resident: ResidentUser,
-    ) {
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Report generated successfully',
+    type: ReportResponseDto,
+  })
+  async generateReport(
+    @Body() dto: CreateReportDto,
+    @AuthenticatedResident() resident: ResidentUser,
+  ) {
+    const response = await this.reportService.generateReport(dto, resident);
+    return new SuccessResponse('Report generated successfully', response.data);
+  }
 
-        const response = await this.reportService.generateReport(dto, resident);
-        return new SuccessResponse('Report generated successfully', response.data);
-    }
+  // getAllReportsByUser
+  @Get()
+  @ApiQuery({ name: 'type', enum: ReportType, required: false })
+  @ApiQuery({ name: 'startDate', required: false })
+  @ApiQuery({ name: 'endDate', required: false })
+  async getReports(
+    @AuthenticatedResident() resident: ResidentUser,
+    @Query() filters: GetReportsDto,
+  ) {
+    const reports = await this.reportService.getReportsByUser(
+      resident,
+      filters,
+    );
+    return new SuccessResponse('Reports fetched', reports);
+  }
 
-
-
-    // getAllReportsByUser
-    @Get()
-    @ApiQuery({ name: 'type', enum: ReportType, required: false })
-    @ApiQuery({ name: 'startDate', required: false })
-    @ApiQuery({ name: 'endDate', required: false })
-    async getReports(@AuthenticatedResident() resident: ResidentUser, @Query() filters: GetReportsDto) {
-        const reports = await this.reportService.getReportsByUser(resident, filters);
-        return new SuccessResponse('Reports fetched', reports);
-    }
-
-    // getReportbyId
-    @Get(':id')
-    @ApiResponse({
-        status: 200,
-        description: 'Report retrieved successfully',
-        type: FullReportResponseDto,
-    })
-    async getReport(
-        @AuthenticatedResident() resident: ResidentUser,
-        @Param('id') id: string,
-    ) {
-        const report = await this.reportService.getReportById(id, resident);
-        return new SuccessResponse('Report retrieved successfully', report);
-    }
+  // getReportbyId
+  @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Report retrieved successfully',
+    type: FullReportResponseDto,
+  })
+  async getReport(
+    @AuthenticatedResident() resident: ResidentUser,
+    @Param('id') id: string,
+  ) {
+    const report = await this.reportService.getReportById(id, resident);
+    return new SuccessResponse('Report retrieved successfully', report);
+  }
 }
