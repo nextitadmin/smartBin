4870ab5f37032b51bfc5404ae08df937a17cfc02
Gabriel Adewumi <codergab@gmail.com>
2025-08-28 19:41:52 +0100

    added ms controllers

---
+++ src/resident/admin.controller.ts
@@ -0,0 +1,19 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { ResidentService } from './resident.service';
+
+@Controller()
+export class AdminResidentController {
+  constructor(private readonly residentService: ResidentService) {}
+  @MessagePattern({
+    cmd: 'GET_APPLICATION_DETAILS',
+  })
+  async getAllResidents(payload: { page?: string; limit?: string }) {
+    console.log('Getting all resident bin applications', payload);
+
+    const pageNumber = parseInt(payload.page ?? '1', 10);
+    const limitNumber = parseInt(payload.limit ?? '10', 10);
+
+    return this.residentService.getAllResidents();
+  }
+}
