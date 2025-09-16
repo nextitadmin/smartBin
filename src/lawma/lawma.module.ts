1012d42c246a6b658c9d81df8531c86022b4ac0b
Gabriel Adewumi <codergab@gmail.com>
2025-09-16 16:50:47 +0100

    updated user types

--- src/lawma/lawma.module.ts
+++ src/lawma/lawma.module.ts
@@ -3,6 +3,7 @@ import { SuperadminsModule } from './superadmins/superadmins.module';
 import { PspModule } from './psp/psp.module';
 import { UsersModule as LawmaUserManagementModule } from './user-management/user-management.module';
 import { LawmaSmartbinsModule } from './smartbins/smartbins.module';
+import { LawmaPartnerModule } from './lawma-partner/lawma-partner.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { LawmaSmartbinsModule } from './smartbins/smartbins.module';
     PspModule,
     LawmaUserManagementModule,
     LawmaSmartbinsModule,
+    LawmaPartnerModule,
   ],
 })
 export class LawmaModule {}
