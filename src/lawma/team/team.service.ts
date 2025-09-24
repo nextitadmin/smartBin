ad8678f0f6808919ef8921b00b2f6ca553d510cd
Gabriel Adewumi <codergab@gmail.com>
2025-09-24 16:05:17 +0100

    team fix

--- src/lawma/team/team.service.ts
+++ src/lawma/team/team.service.ts
@@ -4,7 +4,7 @@ import {
   AdministratorRole,
   AdministratorStatus,
 } from '@models/administrator.model';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateLawmaTeamDto, UpdateLawmaTeamStatusDto } from './dto/team.dto';
@@ -18,13 +18,25 @@ export class TeamService {
   ) {}
 
   async getTeams() {
-    return this.teamMemberModel.find({
-      role: { $ne: AdministratorRole.SuperAdmin },
-      deleted_at: null,
-    });
+    return this.teamMemberModel
+      .find({
+        role: { $ne: AdministratorRole.SuperAdmin },
+        deleted_at: null,
+      })
+      .select('name email phoneNumber role status createdAt');
   }
 
   async createTeam(team: CreateLawmaTeamDto) {
+    const teamMember = await this.teamMemberModel.findOne({
+      email: team.email,
+    });
+
+    if (teamMember) {
+      throw new BadRequestException(
+        'Team member with this email already exists',
+      );
+    }
+
     await this.teamMemberModel.create({
       ...team,
       password: getHashedPassword('password'),
