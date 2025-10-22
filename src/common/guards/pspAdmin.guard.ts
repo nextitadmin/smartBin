668d18a9bb3ae1956ddcf55e35c79ca3148dca77
Gabriel Adewumi <codergab@gmail.com>
2025-10-22 20:04:24 +0100

    lint

--- src/common/guards/pspAdmin.guard.ts
+++ src/common/guards/pspAdmin.guard.ts
@@ -7,7 +7,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { AdminUser, PspAdminUser } from '../types';
+import { PspAdminUser } from '../types';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from './public.guard';
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
@@ -18,7 +18,7 @@ export class PspAdminAuthGuard implements CanActivate {
   constructor(
     @Inject(CACHE_MANAGER) private cacheService: Cache,
     private readonly reflector: Reflector,
-    private readonly authService: PspAuthService
+    private readonly pspAuthService: PspAuthService,
   ) {}
 
   private logger = new Logger(PspAdminAuthGuard.name);
@@ -28,9 +28,6 @@ export class PspAdminAuthGuard implements CanActivate {
       user: Record<string, any>;
       pspAdmin?: PspAdminUser;
     } = ctx.switchToHttp().getRequest();
-
-    console.log("=======Got here ======")
-    console.log(req.pspAdmin);
 
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       ctx.getHandler(),
@@ -46,17 +43,18 @@ export class PspAdminAuthGuard implements CanActivate {
       throw new UnauthorizedException('Not Authorized');
     }
 
-    const administrator = await this.authService.getAdminDetailsByToken(token);
-    if (!true) {
+    const administrator =
+      await this.pspAuthService.getAdminDetailsByToken(token);
+    if (!administrator) {
       this.logger.warn('failed to auth: no user object in request');
       throw new UnauthorizedException('not authenticated!');
     }
 
     req.pspAdmin = {
       id: String(administrator._id),
-      email:administrator.administrator_email,
+      email: administrator.administrator_email,
       name: administrator.administrator_name,
-      token: token
+      token: token,
     };
 
     return true;
--- src/lawma/psp/team-management/teamManagement.controller.ts
+++ src/lawma/psp/team-management/teamManagement.controller.ts
@@ -1,58 +1,73 @@
-import { SuccessResponse } from "@common/http";
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { CreatePspMembersDTO, DeletePspMembersParamDTO, IdDTO, UpdatePspMembersStatusBodyDTO, UpdatePspMembersStatusParamDTO } from "../dto/psp.dto";
-import { ApiTags } from "@nestjs/swagger";
-import { PspTeamManagement } from "./teamManagement.service";
-import { AuthenticatedPspAdmin, PspAdminAuth } from "@common/decorators/auth.decorator";
-import { PspAdminUser } from "@common/types";
+import { SuccessResponse } from '@common/http';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  CreatePspMembersDTO,
+  DeletePspMembersParamDTO,
+  IdDTO,
+  UpdatePspMembersStatusBodyDTO,
+  UpdatePspMembersStatusParamDTO,
+} from '../dto/psp.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { PspTeamManagement } from './teamManagement.service';
+import {
+  AuthenticatedPspAdmin,
+  PspAdminAuth,
+} from '@common/decorators/auth.decorator';
+import { PspAdminUser } from '@common/types';
 
 @ApiTags('PSPs Team Management')
 @Controller({
-    path: 'lawma/psps/team',
-    version: '1'
+  path: 'lawma/psps/team',
+  version: '1',
 })
 @PspAdminAuth()
 export class PspTeamManagementController {
-    constructor(
-        private readonly pspTeamService: PspTeamManagement
-    ) { }
+  constructor(private readonly pspTeamService: PspTeamManagement) {}
 
-     @Post('members')
-      async createPspMembers(
-        @AuthenticatedPspAdmin() pspAdmin: PspAdminUser,
-        @Body() pspMembers: CreatePspMembersDTO,
-      ) {
-        const response = await this.pspTeamService.createPspMembers({
-          psp_id: pspAdmin.id,
-          ...pspMembers,
-        });
-        return new SuccessResponse('psp members created', response);
-      }
+  @Post('members')
+  async createPspMembers(
+    @AuthenticatedPspAdmin() pspAdmin: PspAdminUser,
+    @Body() pspMembers: CreatePspMembersDTO,
+  ) {
+    const response = await this.pspTeamService.createPspMembers({
+      psp_id: pspAdmin.id,
+      ...pspMembers,
+    });
+    return new SuccessResponse('psp members created', response);
+  }
 
-    @Get('members')
-    async getPspMembers(@AuthenticatedPspAdmin() pspAdmin: PspAdminUser) {
-        const response = await this.pspTeamService.getPspMembers(pspAdmin.id);
-        return new SuccessResponse('psp members fetched', response);
-    }
+  @Get('members')
+  async getPspMembers(@AuthenticatedPspAdmin() pspAdmin: PspAdminUser) {
+    const response = await this.pspTeamService.getPspMembers(pspAdmin.id);
+    return new SuccessResponse('psp members fetched', response);
+  }
 
-    @Put('members/:memberId')
-    async updatePspMembersStatus(
-        @AuthenticatedPspAdmin() pspAdmin: PspAdminUser,
-        @Param() param: UpdatePspMembersStatusParamDTO,
-        @Body() body: UpdatePspMembersStatusBodyDTO,
-    ) {
-        const response = await this.pspTeamService.updatePspMembersStatus({
-            pspId: pspAdmin.id,
-            memberId: param.memberId,
-            status: body.status,
-        });
-        return new SuccessResponse('psp members updated', response);
-    }
+  @Put('members/:memberId')
+  async updatePspMembersStatus(
+    @AuthenticatedPspAdmin() pspAdmin: PspAdminUser,
+    @Param() param: UpdatePspMembersStatusParamDTO,
+    @Body() body: UpdatePspMembersStatusBodyDTO,
+  ) {
+    const response = await this.pspTeamService.updatePspMembersStatus({
+      pspId: pspAdmin.id,
+      memberId: param.memberId,
+      status: body.status,
+    });
+    return new SuccessResponse('psp members updated', response);
+  }
 
-    @Delete('members/:memberId')
-    async deletePspMember(@Param() param: DeletePspMembersParamDTO){
-      const response = await this.pspTeamService.deletePspMembers(param.memberId)
+  @Delete('members/:memberId')
+  async deletePspMember(@Param() param: DeletePspMembersParamDTO) {
+    const response = await this.pspTeamService.deletePspMembers(param.memberId);
 
-      return new SuccessResponse('psp member removed successfully', response)
-    }
+    return new SuccessResponse('psp member removed successfully', response);
+  }
 }
